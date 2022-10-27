45591a5554fa931b0badf2a2e5ad814ae915d07a
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-10-27 13:59:10 +0900

    feat: textarea auto resizing

--- components/home/Parser/CleanParser.tsx
+++ components/home/Parser/CleanParser.tsx
@@ -13,15 +13,15 @@ const CleanParser = ({ markDown }: Props) => {
   const { type, innerText } = markDown;
   switch (type) {
     case "p":
-      return <p className={cx("p")}>{innerText}</p>;
+      return <p className={cx("clean", "p")}>{innerText}</p>;
     case "h1":
-      return <h1 className={cx("h1")}>{innerText}</h1>;
+      return <h1 className={cx("clean", "h1")}>{innerText}</h1>;
     case "h2":
-      return <h2 className={cx("h2")}>{innerText}</h2>;
+      return <h2 className={cx("clean", "h2")}>{innerText}</h2>;
     case "h3":
-      return <h3 className={cx("h3")}>{innerText}</h3>;
+      return <h3 className={cx("clean", "h3")}>{innerText}</h3>;
     case "h4":
-      return <h4 className={cx("h4")}>{innerText}</h4>;
+      return <h4 className={cx("clean", "h4")}>{innerText}</h4>;
     default:
       return <div>{innerText}</div>;
   }
--- components/project/EdgeMemo/MemoMarkDown/MarkDownWriter.tsx
+++ components/project/EdgeMemo/MemoMarkDown/MarkDownWriter.tsx
@@ -7,22 +7,37 @@ import {
   handleChangeSelectedInput,
   handleKeyDownMD,
 } from "./MarkDownEventHandler";
+import { useEffect, useRef } from "react";
 
 const cx = classNames.bind(styles);
 
+const resizeTextarea = (ref: HTMLTextAreaElement | null): void => {
+  console.log("abc");
+  if (ref) {
+    console.dir(ref.scrollHeight);
+    ref.style.height = ref.scrollHeight + "px";
+  }
+};
+
 const MemoMarkDownWriter = () => {
+  const ref = useRef<HTMLTextAreaElement>(null);
   const source: MarkDown = useSelectedMarkDown((state) => state.source[0]);
   const input: string = useSelectedMarkDown((state) => state.input);
+
+  useEffect(() => {
+    resizeTextarea(ref.current);
+  }, [input]);
 
   return (
     <li className={cx("MarkDown", "writer", source.type)}>
       <textarea
+        ref={ref}
         className={cx("textArea")}
         placeholder="내용을 입력하세요"
         value={input}
         onBlur={handleBlurMD(source)}
-        onChange={handleChangeSelectedInput}
         onKeyDown={handleKeyDownMD(source, input)}
+        onChange={handleChangeSelectedInput}
         autoFocus
       />
     </li>
